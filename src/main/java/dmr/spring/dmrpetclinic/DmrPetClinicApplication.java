17a4a7ada99bb550897983cc02b3201852b7eb28
daverodal <dave.rodal@gmail.com>
2020-04-01 13:10:27 -0400

    Initial Checkin

---
+++ src/main/java/dmr/spring/dmrpetclinic/DmrPetClinicApplication.java
@@ -0,0 +1,13 @@
+package dmr.spring.dmrpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmrPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmrPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dmr/spring/dmrpetclinic/DmrPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package dmr.spring.dmrpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmrPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
