c1a0b10e1de2bac9ea31b5be95bddc6224a55eaa
daverodal <dave.rodal@gmail.com>
2020-04-01 14:48:10 -0400

    added interfaces for services

---
+++ pet-clinic-data/src/main/java/dmr/spring/dmrpetclinic/services/PetService.java
@@ -0,0 +1,11 @@
+package dmr.spring.dmrpetclinic.services;
+
+import dmr.spring.dmrpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/dmr/spring/dmrpetclinic/services/VetService.java
@@ -0,0 +1,11 @@
+package dmr.spring.dmrpetclinic.services;
+
+import dmr.spring.dmrpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
