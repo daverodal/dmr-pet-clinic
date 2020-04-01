1b462c69863ab3bae42be0fcff4c745055b99ecb
daverodal <dave.rodal@gmail.com>
2020-04-01 15:33:45 -0400

    Refactored Interfaces for common crud interface fixed bug in unit test

--- pet-clinic-data/src/main/java/dmr/spring/dmrpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/dmr/spring/dmrpetclinic/services/OwnerService.java
@@ -1,10 +1,9 @@
 package dmr.spring.dmrpetclinic.services;
 
 import dmr.spring.dmrpetclinic.model.Owner;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.Set;
 
-public interface OwnerService extends CrudRepository<Owner, Long> {
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
 }
--- pet-clinic-data/src/main/java/dmr/spring/dmrpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/dmr/spring/dmrpetclinic/services/PetService.java
@@ -1,9 +1,8 @@
 package dmr.spring.dmrpetclinic.services;
 
 import dmr.spring.dmrpetclinic.model.Pet;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.Set;
 
-public interface PetService extends CrudRepository<Pet, Long> {
+public interface PetService extends CrudService<Pet, Long> {
 }
