f05d883a38decbe9680c34c4f434e38dff0312c8
daverodal <dave.rodal@gmail.com>
2020-04-02 13:11:39 -0400

    added PetType startup service

--- pet-clinic-web/src/main/java/dmr/spring/dmrpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/dmr/spring/dmrpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package dmr.spring.dmrpetclinic.bootstrap;
 
 import dmr.spring.dmrpetclinic.model.Owner;
+import dmr.spring.dmrpetclinic.model.PetType;
 import dmr.spring.dmrpetclinic.model.Vet;
 import dmr.spring.dmrpetclinic.services.OwnerService;
+import dmr.spring.dmrpetclinic.services.PetTypeService;
 import dmr.spring.dmrpetclinic.services.VetService;
 import dmr.spring.dmrpetclinic.services.map.OwnerMapService;
 import dmr.spring.dmrpetclinic.services.map.VetMapService;
@@ -16,15 +18,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType cat = new PetType();
+        cat.setName("cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
+        PetType moose = new PetType();
+        cat.setName("moose");
+        PetType savedMoosePetType = petTypeService.save(moose);
+
         System.out.println("Runninn!");
         Owner owner = new Owner();
         owner.setFirstName("David");
