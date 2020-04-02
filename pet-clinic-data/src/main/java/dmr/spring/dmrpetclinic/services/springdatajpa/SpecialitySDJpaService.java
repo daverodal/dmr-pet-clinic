7ba118b8ebdc897b0ae00e10a9d270d661b3167d
daverodal <dave.rodal@gmail.com>
2020-04-02 17:30:53 -0400

    SpecialitySDJpaService

---
+++ pet-clinic-data/src/main/java/dmr/spring/dmrpetclinic/services/springdatajpa/SpecialitySDJpaService.java
@@ -0,0 +1,47 @@
+package dmr.spring.dmrpetclinic.services.springdatajpa;
+
+import dmr.spring.dmrpetclinic.model.Speciality;
+import dmr.spring.dmrpetclinic.repositories.SpecialityRepository;
+import dmr.spring.dmrpetclinic.services.SpecialityService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialitySDJpaService implements SpecialityService {
+    private final SpecialityRepository specialityRepository;
+
+    public SpecialitySDJpaService(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialityRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return specialityRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialityRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialityRepository.delete(object);
+    }
+
+    @Override
+    public void delteById(Long aLong) {
+        specialityRepository.deleteById(aLong);
+    }
+}
