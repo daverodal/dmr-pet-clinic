9bb42e7ebde027d13fd3ffec4fdbf2c12da03f9d
daverodal <dave.rodal@gmail.com>
2020-04-03 10:28:59 -0400

    VisitMapService

---
+++ pet-clinic-data/src/main/java/dmr/spring/dmrpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package dmr.spring.dmrpetclinic.services;
+
+import dmr.spring.dmrpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/dmr/spring/dmrpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,46 @@
+package dmr.spring.dmrpetclinic.services.map;
+
+import dmr.spring.dmrpetclinic.model.Visit;
+import dmr.spring.dmrpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+    private final VisitService visitService;
+
+    public VisitMapService(VisitService visitService) {
+        this.visitService = visitService;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+        || visit.getPet().getOwner().getId() == null){
+            throw new RuntimeException("Invalid visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+
+    }
+
+    @Override
+    public void delteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
